bc0cd57ba05518734e1298bcaee31eff04c85fba
mugerw96 <mugerwaobadiah96@gmail.com>
2022-05-12 11:21:26 -0700

    init

---
+++ 20_variables.php
@@ -0,0 +1,13 @@
+<?php
+
+$name="mugerwa";
+
+
+// a variable name is assigned a variable value mugerwa
+// $$name $($name)=$mugerwa
+// $mugerwa="pro"
+// echo $mugerwa
+
+$$name="pro";
+echo $$name;
+?>
---
+++ constants.php
@@ -0,0 +1,15 @@
+ <?php
+define('SERVERNAME','localhost',1);
+// echo SERVERNAME;
+// to check the version of php installed
+// echo PHP_VERSION;
+echo "<br>";
+// to check the file where the current files are located
+// echo __FILE__;
+
+
+
+// to get th number of lines 
+// echo __LINE__;
+?> 
+
